e59b128d24ba377b0ad407e947ecebdca1daeb88
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-08-24 14:21:31 -0300

    feat: installing and configuring next-auth for authentication with github

--- src/components/SigInButton/index.tsx
+++ src/components/SigInButton/index.tsx
@@ -1,18 +1,34 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+//useSession() it's a hook that returns tow properties, being data and status. I can use it because I pass the provider on _app, because useSession uses a context to work.
+import { useSession, signIn, signOut } from "next-auth/react";
+// signIn is a next-auth function that authenticates the user
+//signOut is a next-auth function that destroys the current session
 
 import styles from "./styles.module.scss";
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  //status is whether the user is logged in or not. That's what I'm going to use to control.
+  //data is the one that has all the information of the logged in user, such as email, name and avatar.
+  const { status, data } = useSession();
 
-  return isUserLoggedIn ? (
+  return status !== "unauthenticated" ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Samuel Menecucci Moura
-      <FiX color="#737380" />
+      {data?.user.name}
+      <FiX
+        color="#737380"
+        //I pass the singOut without parameter to destroy the current session
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      //in onClick I will trigger a function that triggers signIn. The singIn receives as a parameter the provides that I will use to do the authentication, which here will be github
+      //I could have multiple buttons with different authentication providers. For that I will only need pass the provider that I want to use as a parameter
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       SignIn with Github
     </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
+import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import "../../styles/global.scss";
 import { Header } from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    //SessionProvider from next-auth. This allows me to use useSession() hook in my application*/}
+    <SessionProvider session={pageProps.session}>
       {/* Whenever I need a component to be displayed on all pages, I declare it in  the _app.tsx  */}
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 

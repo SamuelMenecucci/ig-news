3de2f06712d1ddd94b69abc7237fc5731803ae0d
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2022-09-20 11:23:06 -0300

    feat: menu navegation using the Link component of next

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { SignInButton } from "../SigInButton";
 import styles from "./styles.module.scss";
 
@@ -8,10 +9,14 @@ export function Header() {
         <img src="/images/logo.svg" alt="" />
 
         <nav>
-          <a href="" className={styles.active}>
-            Home
-          </a>
-          <a href="">Posts</a>
+          {/* if the href is inside the anchor, when I click on it to display another page, next will load all elements, and the application will be loaded again with all the code, imports and the function from the server. So that doesn't happen, I use the Link and pass the href to it. That way the application will reuse everything that has already been loaded and will not do everything again. That is,it will use the SPA concept. */}
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+
+          <Link href="posts" prefetch>
+            <a>Posts</a>
+          </Link>
         </nav>
         <SignInButton />
       </div>
